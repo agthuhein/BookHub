5da42ef23deaa395da49a731292d957ee2483b2f
Aung Thu Hein <agthuhein.mm@gmail.com>
2025-03-13 18:39:02 +0100

    Added login endpoint

--- src/main/java/com/bookhub/Security/SecurityConfig.java
+++ src/main/java/com/bookhub/Security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig{
                                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/users/register").permitAll()
-//                        .requestMatchers("/api/users/login").permitAll()
+                        .requestMatchers("/api/users/login").permitAll()
                         .requestMatchers("/api/users/getUsers").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
